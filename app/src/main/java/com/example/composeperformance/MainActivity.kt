f601e033f8e7bf130ca9fd21b6d40187f01b3404
taehoonlee <lyc9290@gmail.com>
2022-05-17 12:02:27 +0900

    Compose performnace test

--- app/src/main/java/com/example/composeperformance/MainActivity.kt
+++ app/src/main/java/com/example/composeperformance/MainActivity.kt
@@ -3,17 +3,21 @@ package com.example.composeperformance
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.ScrollState
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.Card
+import androidx.compose.material.ContentAlpha
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.unit.IntOffset
+import androidx.compose.ui.unit.dp
 import kotlinx.coroutines.delay
 import kotlin.random.Random
 
@@ -28,20 +32,14 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun SnackDetail() {
-	Box(modifier = Modifier.fillMaxSize()) {
+	Column(modifier = Modifier.fillMaxSize()) {
 		val scroll = rememberScrollState()
 
-		LaunchedEffect(Unit) {
-			while (true) {
-				delay(1000L)
-				scroll.scrollTo(Random.nextInt(100))
-			}
-		}
-
 		Title(scroll = scroll.value)
-//		RecommendedTitle {
+//		RecommendedTitle1 {
 //			scroll.value
 //		}
+		Content(scroll)
 	}
 }
 
@@ -55,10 +53,34 @@ fun Title(scroll: Int) {
 }
 
 @Composable
-fun RecommendedTitle(scrollProvider: () -> Int) {
+fun RecommendedTitle1(scrollProvider: () -> Int) {
+	val offset = with(LocalDensity.current) { scrollProvider().toDp() }
+
+	Column(modifier = Modifier
+		.offset(y = offset)
+	) {
+		Text("test")
+	}
+}
+
+@Composable
+fun RecommendedTitle2(scrollProvider: () -> Int) {
 	Column(modifier = Modifier
 		.offset { IntOffset(x = 0, y = scrollProvider()) }
 	) {
 		Text("test")
+	}
+}
+
+@Composable
+fun Content(scroll: ScrollState) {
+	Column(modifier = Modifier.fillMaxSize().verticalScroll(scroll)) {
+		repeat(10) {
+			Box(modifier = Modifier
+				.fillMaxWidth()
+				.height(300.dp)
+				.background(Color(Random.nextInt(256), Random.nextInt(256), Random.nextInt(256)))
+			)
+		}
 	}
 }
