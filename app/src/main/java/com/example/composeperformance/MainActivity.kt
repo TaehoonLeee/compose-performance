f532819f957a9af76c86fb4d8edaebdc5ad83c1c
taehoonlee <lyc9290@gmail.com>
2022-05-17 18:31:07 +0900

    Compose performnace test

--- app/src/main/java/com/example/composeperformance/MainActivity.kt
+++ app/src/main/java/com/example/composeperformance/MainActivity.kt
@@ -3,29 +3,33 @@ package com.example.composeperformance
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.animateColor
+import androidx.compose.animation.core.*
 import androidx.compose.foundation.ScrollState
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
-import androidx.compose.material.Card
-import androidx.compose.material.ContentAlpha
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.drawBehind
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalDensity
-import androidx.compose.ui.unit.IntOffset
 import androidx.compose.ui.unit.dp
-import kotlinx.coroutines.delay
 import kotlin.random.Random
 
 class MainActivity : ComponentActivity() {
 	override fun onCreate(savedInstanceState: Bundle?) {
 		super.onCreate(savedInstanceState)
 		setContent {
-			SnackDetail()
+
+			// Read value at Layout Phase
+//			SnackDetail()
+
+			// Read value at Draw Phase
+			AnimatedBox()
 		}
 	}
 }
@@ -49,14 +53,16 @@ fun Title(scroll: Int) {
 		Text("test")
 	}
 
-//	version 1
+	// version 1
+	// Read value at Title Function
 //	val offset = with(LocalDensity.current) { scrollProvider().toDp() }
 //
 //	Column(modifier = Modifier.offset(y = offset)) {
 //
 //	}
 
-//  version 2
+	// version 2
+  	// Read value at Layout Phase
 //	Column(modifier = Modifier
 //		.offset { IntOffset(x = 0, y = scrollProvider()) }
 //	) {
@@ -80,4 +86,22 @@ fun Content(scroll: ScrollState) {
 			)
 		}
 	}
+}
+
+@Composable
+fun AnimatedBox() {
+	val infiniteTransition = rememberInfiniteTransition()
+	val color by infiniteTransition.animateColor(
+		initialValue = Color.Cyan,
+		targetValue = Color.Magenta,
+		animationSpec = infiniteRepeatable(
+			animation = tween(1000, easing = LinearEasing),
+			repeatMode = RepeatMode.Reverse
+		)
+	)
+
+	Box(Modifier.fillMaxSize().background(color))
+
+	// Read value at Draw Phase
+//	Box(Modifier.fillMaxSize().drawBehind { drawRect(color) })
 }
